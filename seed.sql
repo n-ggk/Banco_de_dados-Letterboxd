9f6f94885785e917837fc504e862069c740de622
devNicolas_Pinto <nicolaspinto2003@gmail.com>
2026-07-09 01:02:59 -0300

    Inserindo os primeiros itens no banco de dados

---
+++ seed.sql
@@ -0,0 +1,11 @@
+INSERT INTO usuarios (nome, email, senha, data_cadastro)
+VALUES
+('Asaph', 'asaph@email.com', 'senha', CURRENT_TIMESTAMP),
+('Nicolas', 'nicolas@email.com', '2444666666', CURRENT_TIMESTAMP),
+('Jamilly', 'jamilly@email.com', 'souabarbiegirl', CURRENT_TIMESTAMP);
+
+INSERT INTO filmes (titulo, ano_lancamento, duracao, sinopse)
+VALUES
+('Interestelar', 2014, 169, 'Filme de ficção científica'),
+('Backrooms', 2026, 111, 'Filme de terror'),
+('Batman Begins', 2005, 140, 'Filme de ação');
